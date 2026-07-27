99bed713b8c36f13fc43892551f9b77e3241b803
NetFixa <legoximijoao16@gmail.com>
2026-07-27 12:34:11 +0100

    Create sites.js

---
+++ js/sites.js
@@ -0,0 +1,43 @@
+const sites = [
+
+{
+    name: "Radio Garden",
+    category: "Explorar",
+    description: "Ouve rádios em direto de qualquer parte do mundo.",
+    time: "10 min",
+    url: "https://radio.garden"
+},
+
+{
+    name: "Google Arts & Culture",
+    category: "Explorar",
+    description: "Visita museus e coleções de todo o mundo.",
+    time: "15 min",
+    url: "https://artsandculture.google.com"
+},
+
+{
+    name: "NASA APOD",
+    category: "Aprender",
+    description: "Descobre a imagem astronómica do dia.",
+    time: "5 min",
+    url: "https://apod.nasa.gov/apod/astropix.html"
+},
+
+{
+    name: "Wikipedia Aleatória",
+    category: "Aprender",
+    description: "Lê um artigo escolhido ao acaso.",
+    time: "10 min",
+    url: "https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria"
+},
+
+{
+    name: "GeoGuessr",
+    category: "Jogar",
+    description: "Tenta descobrir onde estás no mundo.",
+    time: "15 min",
+    url: "https://www.geoguessr.com/free"
+}
+
+];
